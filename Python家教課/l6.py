add19e177ac7018e393ebbd1ff6947037ada0dff
Hank-hsieh <53846130+Hank-hsieh@users.noreply.github.com>
2019-08-07 11:30:20 +0800

    Add files via upload

---
+++ Python家教課/l6.py
@@ -0,0 +1,14 @@
+list_a = ['Hello', 37.5, 8]
+print(type(list_a))
+
+
+print(list_a[1])
+
+
+intro = {'firstName' : 'Hank', 
+         'lastName' : ('Hsieh'), 
+         'hobbies' : {'first' : 'music', 
+                      'second' : 'swim', 
+                      'third' : 'game'},
+         'degree' : ['senior', 'junior']}
+print(intro)
